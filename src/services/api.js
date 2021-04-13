8a80eedfe07295140317704204b78f13872c0619
Matheus Costa <matheus.costa@zukk.com.br>
2021-04-13 02:49:31 -0300

    feat: creating service

---
+++ src/services/api.js
@@ -0,0 +1,9 @@
+/* eslint-disable import/prefer-default-export */
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: 'http://localhost:3333/api',
+  headers: { 'Content-Type': 'application/json' }
+});
+
+export { api };
