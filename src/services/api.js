258a30258d68c6650516fddbfcf261a5b3e7c056
Matheus Costa <matheus.costa@zukk.com.br>
2021-04-16 21:35:35 -0300

    feat: using aws url

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3333/api',
+  baseURL: 'http://18.230.65.157:3333/api',
   headers: { 'Content-Type': 'application/json' }
 });
 
