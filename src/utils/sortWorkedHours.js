ad21ebc216e466cd926d89bf3cbc1633c2377206
Matheus Costa <matheus271200.ms@gmail.com>
2021-05-16 18:43:21 -0300

    fix: sorting issue

--- src/utils/sortWorkedHours.js
+++ src/utils/sortWorkedHours.js
@@ -2,7 +2,7 @@ const sortWorkedHours = (a, b) => {
   if (Object.entries(a)[0][1] > Object.entries(b)[0][1]) {
     return -1;
   }
-  if (Object.entries(b)[0][1] < Object.entries(a)[0][1]) {
+  if (Object.entries(a)[0][1] < Object.entries(b)[0][1]) {
     return 1;
   }
   return 0;
